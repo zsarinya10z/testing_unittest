41f6fb495a9b64b0f256f3b75c3651ab304307b2
tumtpk <pk.prateep@gmail.com>
2020-09-21 14:16:40 +0700

    init testcase

--- src/test/java/com/unittest/testing/AppTest.java
+++ src/test/java/com/unittest/testing/AppTest.java
@@ -19,62 +19,69 @@ import org.junit.Test;
 
 public class AppTest 
 {
+	
 	@Test
-	  public void testAssertArrayEquals() {
-	    byte[] expected = "trial".getBytes();
-	    byte[] actual = "trial".getBytes();
-	    assertArrayEquals("failure - byte arrays not same", expected, actual);
-	  }
-
-	  @Test
-	  public void testAssertEquals() {
-	    assertEquals("failure - strings are not equal", "text", "text");
-	  }
-
-	  @Test
-	  public void testAssertFalse() {
-	    assertFalse("failure - should be false", false);
-	  }
-
-	  @Test
-	  public void testAssertNotNull() {
-	    assertNotNull("should not be null", new Object());
-	  }
-
-	  @Test
-	  public void testAssertNotSame() {
-	    assertNotSame("should not be same Object", new Object(), new Object());
-	  }
-
-	  @Test
-	  public void testAssertNull() {
-	    assertNull("should be null", null);
-	  }
-
-	  @Test
-	  public void testAssertSame() {
-	    Integer aNumber = Integer.valueOf(768);
-	    assertSame("should be same", aNumber, aNumber);
-	  }
-
-	  // JUnit Matchers assertThat
-	  @Test
-	  public void testAssertThatBothContainsString() {
-	    assertThat("albumen", both(containsString("a")).and(containsString("b")));
-	  }
-
-	  @Test
-	  public void testAssertThatHasItems() {
-	    assertThat(Arrays.asList("one", "two", "three"), hasItems("one", "three"));
-	  }
-
-	  @Test
-	  public void testAssertThatEveryItemContainsString() {
-	    assertThat(Arrays.asList(new String[] { "fun", "ban", "net" }), everyItem(containsString("n")));
-	  }
-
-	  @Test
-	  public void testAssertTrue() {
-	    assertTrue("failure - should be true", true);
-	  }
+	public void pub() {
+		assertEquals(0, 1);
+	}
+//	@Test
+//	  public void testAssertArrayEquals() {
+//	    byte[] expected = "trial".getBytes();
+//	    byte[] actual = "trial".getBytes();
+//	    assertArrayEquals("failure - byte arrays not same", expected, actual);
+//	  }
+//
+//	  @Test
+//	  public void testAssertEquals() {
+//	    assertEquals("failure - strings are not equal", "text", "text");
+//	  }
+//
+//	  @Test
+//	  public void testAssertFalse() {
+//	    assertFalse("failure - should be false", false);
+//	  }
+//
+//	  @Test
+//	  public void testAssertNotNull() {
+//	    assertNotNull("should not be null", new Object());
+//	  }
+//
+//	  @Test
+//	  public void testAssertNotSame() {
+//	    assertNotSame("should not be same Object", new Object(), new Object());
+//	  }
+//
+//	  @Test
+//	  public void testAssertNull() {
+//	    assertNull("should be null", null);
+//	  }
+//
+//	  @Test
+//	  public void testAssertSame() {
+//	    Integer aNumber = Integer.valueOf(768);
+//	    assertSame("should be same", aNumber, aNumber);
+//	  }
+//
+//	  // JUnit Matchers assertThat
+//	  @Test
+//	  public void testAssertThatBothContainsString() {
+//	    assertThat("albumen", both(containsString("a")).and(containsString("b")));
+//	  }
+//
+//	  @Test
+//	  public void testAssertThatHasItems() {
+//	    assertThat(Arrays.asList("one", "two", "three"), hasItems("one", "three"));
+//	  }
+//
+//	  @Test
+//	  public void testAssertThatEveryItemContainsString() {
+//	    assertThat(Arrays.asList(new String[] { "fun", "ban", "net" }), everyItem(containsString("n")));
+//	  }
+//
+//	  @Test
+//	  public void testAssertTrue() {
+//	    assertTrue("failure - should be true", true);
+//	  }
+	  
+	  
 }
