a93b06b6effbe6f2d92718fce91472a78bb68336
zsarinya10z <zpaatz10@gmail.com>
2020-09-28 13:39:00 +0700

    testcese

--- src/main/java/com/unittest/testing/fahrenheit.java
+++ src/main/java/com/unittest/testing/fahrenheit.java
@@ -1,19 +1,23 @@
 package com.unittest.testing;
-public class fahrenheit {
-	 private double celcius;
-	 public double getCelcius() {
+
+public class Fahrenheit {
+	private double celcius;
+	
+	public double getCelcius() {
 		return celcius;
-	 }
-	 public void input(double celcius) {
-		 this.celcius = celcius;
-	 }
-	 public boolean isBound(double celcius) {
-		 boolean isResult;
-		 if(celcius >= -1000 && celcius <= 1000) {
-			 isResult = true;
-		 }else {
-			 isResult = false;
-		 }
-		 return isResult;
-	 }
+	}
+
+	public void input(double celcius) {
+		this.celcius = celcius;
+	}
+	
+	public boolean isBound(double celcius) {
+		boolean isResult;
+		if(celcius >= -1000 && celcius <= 1000) {
+			isResult = true;
+		}else{
+			isResult = false;
+		}
+		return isResult;
+	}
 }
